7f242544aa3d108ded5072b27304040c0769a82d
Sidharth Sreeram <sidharthsreeram@gmail.com>
2026-06-29 17:41:37 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Sidharth-82.github.io',
-  base: '/Portfolio-Website',
+  base: '/portfolio',
   integrations: [
     react(),
     // We import our own global.css (with the @tailwind directives + theme
